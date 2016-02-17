b26292b71c89df80cea64167f18080f8f0e24d59
Scott <scottalankatz@gmail.com>
2016-02-16 23:23:12 -0500

    adjusting layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
+  post 'create_user' => 'users#create', as: :create_user
+
   devise_for :users, controllers: {registrations: 'registrations'}
+
+  resources :users, except: :create do
+  end
 
   root 'welcome#index'
 
