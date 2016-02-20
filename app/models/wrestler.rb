2e3f8a5a9c3f869dde77962fbfbd2c39172df3cf
Scott <scottalankatz@gmail.com>
2016-02-20 15:43:44 -0500

    downloadable csv

--- app/models/wrestler.rb
+++ app/models/wrestler.rb
@@ -23,9 +23,9 @@ class Wrestler < ActiveRecord::Base
   def self.to_csv2(options = {})
 
     CSV.generate(options) do |csv|
-      csv << ["Weight", "league place", "school", "full name", "grade", "wins", "losses", "section place", "state place", "tournament 1", "tournament 2", "tournament 3", "tournament 4", "tournament 5", "h2h 1", "h2h 2", "h2h 3", "h2h 4", "h2h 5"]
+      csv << ["Weight", "league place", "school", "full name", "grade", "wins", "losses", "section place", "state place", "T1", "T1 PLACE", "T2", "T2 PLACE", "T3", "T3 PLACE", "T4", "T4 PLACE", "T5", "T5 PLACE", "h2h 1", "h2h 1R", "h2h 2", "h2h 2R", "h2h 3", "h2h 3R", "h2h 4", "h2h 4R", "h2h 5", "h2h 5R"]
       all.each do |wrestler|
-        csv << [wrestler.weight, wrestler.league_place, wrestler.school.name, wrestler.first_name + " " + wrestler.last_name, wrestler.grade, wrestler.wins, wrestler.losses, wrestler.section_place, wrestler.state_place, wrestler.t1_name , wrestler.t2_name  , wrestler.t3_name   , wrestler.t4_name  , wrestler.t5_name  , wrestler.h2h_1 , wrestler.h2h_2 , wrestler.h2h_3 , wrestler.h2h_4 , wrestler.h2h_5  ]
+        csv << [wrestler.weight, wrestler.league_place, wrestler.school.name, wrestler.first_name + " " + wrestler.last_name, wrestler.grade, wrestler.wins, wrestler.losses, wrestler.section_place, wrestler.state_place, wrestler.t1_name, wrestler.t1_place, wrestler.t2_name, wrestler.t2_place, wrestler.t3_name, wrestler.t3_place, wrestler.t4_name, wrestler.t4_place, wrestler.t5_name, wrestler.t5_place, wrestler.h2h_1, wrestler.h2h_r1, wrestler.h2h_2, wrestler.h2h_r2, wrestler.h2h_3, wrestler.h2h_r3,  wrestler.h2h_4, wrestler.h2h_r4, wrestler.h2h_5, wrestler.h2h_r5 ]
       end
     end
   end
