7e7dd7883ca8339e74ab0574d65f7b1ee4b1030e
Scott <scottalankatz@gmail.com>
2016-02-25 09:50:39 -0500

    added live streaming link

--- db/migrate/20160224001354_create_messages.rb
+++
@@ -1,9 +0,0 @@
-class CreateMessages < ActiveRecord::Migration
-  def change
-    create_table :messages do |t|
-       t.timestamps
-       t.string :msg
-       t.references :conversation, index: true
-    end
-  end
-end
