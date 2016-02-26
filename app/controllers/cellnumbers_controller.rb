b0f3ebf1b7de2a0114c3d722633dfd5709d2be7c
Scott <scottalankatz@gmail.com>
2016-02-26 14:06:40 -0500

    alerts can't be sent more than once

--- app/controllers/cellnumbers_controller.rb
+++ app/controllers/cellnumbers_controller.rb
@@ -82,378 +82,466 @@ class CellnumbersController < ApplicationController
 
   def send_alert_106
     @numbers = Cellnumber.where(:alert_106 => true)
-    shift_recent_alert
+    find_alerts
+    if @alert1.message != "106 CHAMPIONSHIP"
     @alert = Alert.find(1)
+    shift_recent_alert
     @alert.message = "106 CHAMPIONSHIP"
     @alert.save
     easy = SMSEasy::Client.new
     @numbers.each do |number|
       easy.deliver("#{number.cell}", "#{number.carrier_name}", "106 #{MESSAGE}  To unsubscribe,visit www.ncswrestling.com/cellnumbers/#{number.id}")
     end
+  end
     redirect_to cellnumbers_path
   end
 
   def send_alert_106_cons
     @numbers = Cellnumber.where(:alert_106 => true)
-    shift_recent_alert
+    find_alerts
+    if @alert1.message != "106 CONSOLATION"
     @alert = Alert.find(1)
+    shift_recent_alert
     @alert.message = "106 CONSOLATION"
     @alert.save
     easy = SMSEasy::Client.new
     @numbers.each do |number|
       easy.deliver("#{number.cell}", "#{number.carrier_name}", "106 #{MESSAGE_B}  To unsubscribe,visit www.ncswrestling.com/cellnumbers/#{number.id}")
     end
+  end
     redirect_to cellnumbers_path
   end
 
   def send_alert_113
     @numbers = Cellnumber.where(:alert_113 => true)
-    shift_recent_alert
+    find_alerts
+    if @alert1.message != "113 CHAMPIONSHIP"
     @alert = Alert.find(1)
+    shift_recent_alert
     @alert.message = "113 CHAMPIONSHIP"
     @alert.save
     easy = SMSEasy::Client.new
     @numbers.each do |number|
       easy.deliver("#{number.cell}", "#{number.carrier_name}", "113 #{MESSAGE}  To unsubscribe,visit www.ncswrestling.com/cellnumbers/#{number.id}")
     end
+  end
     redirect_to cellnumbers_path
   end
 
   def send_alert_113_cons
     @numbers = Cellnumber.where(:alert_113 => true)
-    shift_recent_alert
+    find_alerts
+    if @alert1.message != "113 CONSOLATION"
     @alert = Alert.find(1)
+    shift_recent_alert
     @alert.message = "113 CONSOLATION"
     @alert.save
     easy = SMSEasy::Client.new
     @numbers.each do |number|
       easy.deliver("#{number.cell}", "#{number.carrier_name}", "113 #{MESSAGE_B}  To unsubscribe,visit www.ncswrestling.com/cellnumbers/#{number.id}")
     end
+  end
     redirect_to cellnumbers_path
   end
 
   def send_alert_120
     @numbers = Cellnumber.where(:alert_120 => true)
-    shift_recent_alert
+    find_alerts
+    if @alert1.message != "120 CHAMPIONSHIP"
     @alert = Alert.find(1)
+    shift_recent_alert
     @alert.message = "120 CHAMPIONSHIP"
     @alert.save
     easy = SMSEasy::Client.new
     @numbers.each do |number|
       easy.deliver("#{number.cell}", "#{number.carrier_name}", "120 #{MESSAGE}  To unsubscribe,visit www.ncswrestling.com/cellnumbers/#{number.id}")
     end
+  end
     redirect_to cellnumbers_path
   end
 
   def send_alert_120_cons
     @numbers = Cellnumber.where(:alert_120 => true)
-    shift_recent_alert
+    find_alerts
+    if @alert1.message != "120 CONSOLATION"
     @alert = Alert.find(1)
+    shift_recent_alert
     @alert.message = "120 CONSOLATION"
     @alert.save
     easy = SMSEasy::Client.new
     @numbers.each do |number|
       easy.deliver("#{number.cell}", "#{number.carrier_name}", "120 #{MESSAGE_B}  To unsubscribe,visit www.ncswrestling.com/cellnumbers/#{number.id}")
     end
+  end
     redirect_to cellnumbers_path
   end
 
   def send_alert_126
     @numbers = Cellnumber.where(:alert_126 => true)
-    shift_recent_alert
+    find_alerts
+    if @alert1.message != "126 CHAMPIONSHIP"
     @alert = Alert.find(1)
+    shift_recent_alert
     @alert.message = "126 CHAMPIONSHIP"
     @alert.save
     easy = SMSEasy::Client.new
     @numbers.each do |number|
       easy.deliver("#{number.cell}", "#{number.carrier_name}", "126 #{MESSAGE}  To unsubscribe,visit www.ncswrestling.com/cellnumbers/#{number.id}")
     end
+  end
     redirect_to cellnumbers_path
   end
 
   def send_alert_126_cons
     @numbers = Cellnumber.where(:alert_126 => true)
-    shift_recent_alert
+    find_alerts
+    if @alert1.message != "126 CONSOLATION"
     @alert = Alert.find(1)
+    shift_recent_alert
     @alert.message = "126 CONSOLATION"
     @alert.save
     easy = SMSEasy::Client.new
     @numbers.each do |number|
       easy.deliver("#{number.cell}", "#{number.carrier_name}", "126 #{MESSAGE_B}  To unsubscribe,visit www.ncswrestling.com/cellnumbers/#{number.id}")
     end
+  end
     redirect_to cellnumbers_path
   end
 
 
   def send_alert_132
     @numbers = Cellnumber.where(:alert_132 => true)
-    shift_recent_alert
+    find_alerts
+    if @alert1.message != "132 CHAMPIONSHIP"
     @alert = Alert.find(1)
+    shift_recent_alert
     @alert.message = "132 CHAMPIONSHIP"
     @alert.save
     easy = SMSEasy::Client.new
     @numbers.each do |number|
       easy.deliver("#{number.cell}", "#{number.carrier_name}", "132 #{MESSAGE}  To unsubscribe,visit www.ncswrestling.com/cellnumbers/#{number.id}")
     end
+  end
     redirect_to cellnumbers_path
   end
 
   def send_alert_132_cons
     @numbers = Cellnumber.where(:alert_132 => true)
-    shift_recent_alert
+    find_alerts
+    if @alert1.message != "132 CONSOLATION"
     @alert = Alert.find(1)
+    shift_recent_alert
     @alert.message = "132 CONSOLATION"
     @alert.save
     easy = SMSEasy::Client.new
     @numbers.each do |number|
       easy.deliver("#{number.cell}", "#{number.carrier_name}", "132 #{MESSAGE_B}  To unsubscribe,visit www.ncswrestling.com/cellnumbers/#{number.id}")
     end
+  end
     redirect_to cellnumbers_path
   end
 
   def send_alert_138
     @numbers = Cellnumber.where(:alert_138 => true)
-    shift_recent_alert
+    find_alerts
+    if @alert1.message != "138 CHAMPIONSHIP"
     @alert = Alert.find(1)
+    shift_recent_alert
     @alert.message = "138 CHAMPIONSHIP"
     @alert.save
     easy = SMSEasy::Client.new
     @numbers.each do |number|
       easy.deliver("#{number.cell}", "#{number.carrier_name}", "138 #{MESSAGE}  To unsubscribe,visit www.ncswrestling.com/cellnumbers/#{number.id}")
     end
+  end
     redirect_to cellnumbers_path
   end
 
   def send_alert_138_cons
     @numbers = Cellnumber.where(:alert_138 => true)
-    shift_recent_alert
+    find_alerts
+    if @alert1.message != "138 CONSOLATION"
     @alert = Alert.find(1)
+    shift_recent_alert
     @alert.message = "138 CONSOLATION"
     @alert.save
     easy = SMSEasy::Client.new
     @numbers.each do |number|
       easy.deliver("#{number.cell}", "#{number.carrier_name}", "138 #{MESSAGE_B}  To unsubscribe,visit www.ncswrestling.com/cellnumbers/#{number.id}")
     end
+  end
     redirect_to cellnumbers_path
   end
 
   def send_alert_145
     @numbers = Cellnumber.where(:alert_145 => true)
-    shift_recent_alert
+    find_alerts
+    if @alert1.message != "145 CHAMPIONSHIP"
     @alert = Alert.find(1)
+    shift_recent_alert
     @alert.message = "145 CHAMPIONSHIP"
     @alert.save
     easy = SMSEasy::Client.new
     @numbers.each do |number|
       easy.deliver("#{number.cell}", "#{number.carrier_name}", "145 #{MESSAGE}  To unsubscribe,visit www.ncswrestling.com/cellnumbers/#{number.id}")
     end
+  end
     redirect_to cellnumbers_path
   end
 
   def send_alert_145_cons
     @numbers = Cellnumber.where(:alert_145 => true)
-    shift_recent_alert
+    find_alerts
+    if @alert1.message != "145 CONSOLATION"
     @alert = Alert.find(1)
+    shift_recent_alert
     @alert.message = "145 CONSOLATION"
     @alert.save
     easy = SMSEasy::Client.new
     @numbers.each do |number|
       easy.deliver("#{number.cell}", "#{number.carrier_name}", "145 #{MESSAGE_B}  To unsubscribe,visit www.ncswrestling.com/cellnumbers/#{number.id}")
     end
+  end
     redirect_to cellnumbers_path
   end
 
   def send_alert_152
     @numbers = Cellnumber.where(:alert_152 => true)
-    shift_recent_alert
+    find_alerts
+    if @alert1.message != "152 CHAMPIONSHIP"
     @alert = Alert.find(1)
+    shift_recent_alert
     @alert.message = "152 CHAMPIONSHIP"
     @alert.save
     easy = SMSEasy::Client.new
     @numbers.each do |number|
       easy.deliver("#{number.cell}", "#{number.carrier_name}", "152 #{MESSAGE}  To unsubscribe,visit www.ncswrestling.com/cellnumbers/#{number.id}")
     end
+  end
     redirect_to cellnumbers_path
   end
 
   def send_alert_152_cons
     @numbers = Cellnumber.where(:alert_152 => true)
-    shift_recent_alert
+    find_alerts
+    if @alert1.message != "152 CONSOLATION"
     @alert = Alert.find(1)
+    shift_recent_alert
     @alert.message = "152 CONSOLATION"
     @alert.save
     easy = SMSEasy::Client.new
     @numbers.each do |number|
       easy.deliver("#{number.cell}", "#{number.carrier_name}", "152 #{MESSAGE_B}  To unsubscribe,visit www.ncswrestling.com/cellnumbers/#{number.id}")
     end
+  end
     redirect_to cellnumbers_path
   end
 
   def send_alert_160
     @numbers = Cellnumber.where(:alert_160 => true)
-    shift_recent_alert
+    find_alerts
+    if @alert1.message != "160 CHAMPIONSHIP"
     @alert = Alert.find(1)
+    shift_recent_alert
     @alert.message = "160 CHAMPIONSHIP"
     @alert.save
     easy = SMSEasy::Client.new
     @numbers.each do |number|
       easy.deliver("#{number.cell}", "#{number.carrier_name}", "160 #{MESSAGE}  To unsubscribe,visit www.ncswrestling.com/cellnumbers/#{number.id}")
     end
+  end
     redirect_to cellnumbers_path
   end
 
   def send_alert_160_cons
     @numbers = Cellnumber.where(:alert_160 => true)
-    shift_recent_alert
+    find_alerts
+    if @alert1.message != "160 CONSOLATION"
     @alert = Alert.find(1)
+    shift_recent_alert
     @alert.message = "160 CONSOLATION"
     @alert.save
     easy = SMSEasy::Client.new
     @numbers.each do |number|
       easy.deliver("#{number.cell}", "#{number.carrier_name}", "160 #{MESSAGE_B}  To unsubscribe,visit www.ncswrestling.com/cellnumbers/#{number.id}")
     end
+  end
     redirect_to cellnumbers_path
   end
 
   def send_alert_170
     @numbers = Cellnumber.where(:alert_170 => true)
-    shift_recent_alert
+    find_alerts
+    if @alert1.message != "170 CHAMPIONSHIP"
     @alert = Alert.find(1)
+    shift_recent_alert
     @alert.message = "170 CHAMPIONSHIP"
     @alert.save
     easy = SMSEasy::Client.new
     @numbers.each do |number|
       easy.deliver("#{number.cell}", "#{number.carrier_name}", "170 #{MESSAGE}  To unsubscribe,visit www.ncswrestling.com/cellnumbers/#{number.id}")
     end
+  end
     redirect_to cellnumbers_path
   end
 
   def send_alert_170_cons
     @numbers = Cellnumber.where(:alert_170 => true)
-    shift_recent_alert
+    find_alerts
+    if @alert1.message != "170 CONSOLATION"
     @alert = Alert.find(1)
+    shift_recent_alert
     @alert.message = "170 CONSOLATION"
     @alert.save
     easy = SMSEasy::Client.new
     @numbers.each do |number|
       easy.deliver("#{number.cell}", "#{number.carrier_name}", "170 #{MESSAGE_B}  To unsubscribe,visit www.ncswrestling.com/cellnumbers/#{number.id}")
     end
+  end
     redirect_to cellnumbers_path
   end
 
   def send_alert_182
     @numbers = Cellnumber.where(:alert_182 => true)
-    shift_recent_alert
+    find_alerts
+    if @alert1.message != "182 CHAMPIONSHIP"
     @alert = Alert.find(1)
+    shift_recent_alert
     @alert.message = "182 CHAMPIONSHIP"
     @alert.save
     easy = SMSEasy::Client.new
     @numbers.each do |number|
       easy.deliver("#{number.cell}", "#{number.carrier_name}", "182 #{MESSAGE}  To unsubscribe,visit www.ncswrestling.com/cellnumbers/#{number.id}")
     end
+  end
     redirect_to cellnumbers_path
   end
 
   def send_alert_182_cons
     @numbers = Cellnumber.where(:alert_182 => true)
-    shift_recent_alert
+    find_alerts
+    if @alert1.message != "182 CONSOLATION"
     @alert = Alert.find(1)
+    shift_recent_alert
     @alert.message = "182 CONSOLATION"
     @alert.save
     easy = SMSEasy::Client.new
     @numbers.each do |number|
       easy.deliver("#{number.cell}", "#{number.carrier_name}", "182 #{MESSAGE_B}  To unsubscribe,visit www.ncswrestling.com/cellnumbers/#{number.id}")
     end
+  end
     redirect_to cellnumbers_path
   end
 
   def send_alert_195
     @numbers = Cellnumber.where(:alert_195 => true)
-    shift_recent_alert
+    find_alerts
+    if @alert1.message != "195 CHAMPIONSHIP"
     @alert = Alert.find(1)
+    shift_recent_alert
     @alert.message = "195 CHAMPIONSHIP"
     @alert.save
     easy = SMSEasy::Client.new
     @numbers.each do |number|
       easy.deliver("#{number.cell}", "#{number.carrier_name}", "195 #{MESSAGE}  To unsubscribe,visit www.ncswrestling.com/cellnumbers/#{number.id}")
     end
+  end
     redirect_to cellnumbers_path
   end
 
   def send_alert_195_cons
     @numbers = Cellnumber.where(:alert_195 => true)
-    shift_recent_alert
+    find_alerts
+    if @alert1.message != "195 CONSOLATION"
     @alert = Alert.find(1)
+    shift_recent_alert
     @alert.message = "195 CONSOLATION"
     @alert.save
     easy = SMSEasy::Client.new
     @numbers.each do |number|
       easy.deliver("#{number.cell}", "#{number.carrier_name}", "195 #{MESSAGE_B}  To unsubscribe,visit www.ncswrestling.com/cellnumbers/#{number.id}")
     end
+  end
     redirect_to cellnumbers_path
   end
 
   def send_alert_220
     @numbers = Cellnumber.where(:alert_220 => true)
-    shift_recent_alert
+    find_alerts
+    if @alert1.message != "220 CHAMPIONSHIP"
     @alert = Alert.find(1)
+    shift_recent_alert
     @alert.message = "220 CHAMPIONSHIP"
     @alert.save
     easy = SMSEasy::Client.new
     @numbers.each do |number|
       easy.deliver("#{number.cell}", "#{number.carrier_name}", "220 #{MESSAGE}  To unsubscribe,visit www.ncswrestling.com/cellnumbers/#{number.id}")
     end
+  end
     redirect_to cellnumbers_path
   end
 
   def send_alert_220_cons
     @numbers = Cellnumber.where(:alert_220 => true)
-    shift_recent_alert
+    find_alerts
+    if @alert1.message != "220 CONSOLATION"
     @alert = Alert.find(1)
+    shift_recent_alert
     @alert.message = "220 CONSOLATION"
     @alert.save
     easy = SMSEasy::Client.new
     @numbers.each do |number|
       easy.deliver("#{number.cell}", "#{number.carrier_name}", "220 #{MESSAGE_B}  To unsubscribe,visit www.ncswrestling.com/cellnumbers/#{number.id}")
     end
+  end
     redirect_to cellnumbers_path
   end
 
   def send_alert_285
     @numbers = Cellnumber.where(:alert_285 => true)
-    shift_recent_alert
+    find_alerts
+    if @alert1.message != "285 CHAMPIONSHIP"
     @alert = Alert.find(1)
+    shift_recent_alert
     @alert.message = "285 CHAMPIONSHIP"
     @alert.save
     easy = SMSEasy::Client.new
     @numbers.each do |number|
       easy.deliver("#{number.cell}", "#{number.carrier_name}", "285 #{MESSAGE}  To unsubscribe,visit www.ncswrestling.com/cellnumbers/#{number.id}")
     end
+  end
     redirect_to cellnumbers_path
   end
 
   def send_alert_285_cons
     @numbers = Cellnumber.where(:alert_285 => true)
-    shift_recent_alert
-    @alert = Alert.find(1)
-    @alert.message = "285 CONSOLATION"
-    @alert.save
-    easy = SMSEasy::Client.new
-    @numbers.each do |number|
-      easy.deliver("#{number.cell}", "#{number.carrier_name}", "285 #{MESSAGE_B}  To unsubscribe,visit www.ncswrestling.com/cellnumbers/#{number.id}")
+    find_alerts
+
+    if @alert1.message != "285 CONSOLATION"
+      @alert = Alert.find(1)
+      shift_recent_alert
+      @alert.message = "285 CONSOLATION"
+      @alert.save
+      easy = SMSEasy::Client.new
+      @numbers.each do |number|
+        easy.deliver("#{number.cell}", "#{number.carrier_name}", "285 #{MESSAGE_B}  To unsubscribe,visit www.ncswrestling.com/cellnumbers/#{number.id}")
+      end
     end
     redirect_to cellnumbers_path
   end
 
   private
 
-  def shift_recent_alert
+  def find_alerts
     @alert3 = Alert.find(3)
     @alert2 = Alert.find(2)
     @alert1 = Alert.find(1)
+  end
+
+  def shift_recent_alert
     @alert3.message = @alert2.message
     @alert3.save
-    @alert2.message = @alert1.message
+    @alert2.message = @alert.message
     @alert2.save
   end
 
