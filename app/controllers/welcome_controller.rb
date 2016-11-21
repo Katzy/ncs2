3a9858e7cb1f7eddaf4cdfe45470ba684c846eda
Scott <scottalankatz@gmail.com>
2016-11-21 11:21:26 -0500

    added wrestler show page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,9 +2,9 @@ class WelcomeController < ApplicationController
 
   def index
     @user = current_user
-    @alert = Alert.find(1)
-    @alert2 = Alert.find(2)
-    @alert3 = Alert.find(3)
+    # @alert = Alert.find(1)
+    # @alert2 = Alert.find(2)
+    # @alert3 = Alert.find(3)
   end
 
 end
