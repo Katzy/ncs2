3be307755b9f2ce1549acfb6ec2b2e04ce266292
Scott <scottalankatz@gmail.com>
2017-02-20 13:36:40 -0500

    fixing sort

--- app/controllers/wrestlers_controller.rb
+++ app/controllers/wrestlers_controller.rb
@@ -230,8 +230,9 @@ class WrestlersController < ApplicationController
 
 
   def select_wrestlers(wt)
+      params1 = ["#{wt}", false]
       params = ["1","2","3","4","5", true]
-      @w = Wrestler.where("weight = #{wt} AND scratch = #{false}")
+      @w = Wrestler.where("weight = ? AND scratch = ?", *params1)
       @wrestlers = @w.where("league_place = ? OR league_place = ? OR league_place = ? OR league_place = ? OR league_place = ? OR alternate = ?", *params).order('weight ASC, seed ASC, state_place ASC, section_place ASC, seed ASC, wins DESC')
       wrestlers = @wrestlers 
       # Wrestler.where("weight = #{wt}").order('weight ASC, seed ASC, wins DESC')  # for csv format
