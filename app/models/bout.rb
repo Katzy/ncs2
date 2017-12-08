889349a015f445665a358c1725bf05cdce272b14
Scott <scottalankatz@gmail.com>
2017-12-08 13:27:47 -0500

    fixed bout sheet

--- app/models/bout.rb
+++ app/models/bout.rb
@@ -8,7 +8,7 @@ class Bout < ActiveRecord::Base
   validates :dual_or_tourney, presence: true, null: false
   
   # validates :opponent_team, presence: true, null: false
-  
+  validates :opponent_team, presence: true, null: false
   validates :opponent_name, presence: true, null: false
   validates :win_loss, presence: true, null: false
   validates :result_type, presence: true, null: false
