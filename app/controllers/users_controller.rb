44f83f1eaedfc1b3dd1fb021158ef055beba44ee
Scott <scottalankatz@gmail.com>
2020-02-14 07:29:54 -0500

    changed new user mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -78,7 +78,7 @@ class UsersController < ApplicationController
       # @team.school = @user.school
       # @team.user_id = @user.id
       # @team = Team.create(team_params)
-       # UserMailer.new_user(@user).deliver
+       UserMailer.new_user(@user).deliver
        UserMailer.new_user_added(@user).deliver
       redirect_to root_url, notice: "User was successfully created"
     else
